02fd6b7e19538d6dc36bbce1acc29e4d78f27b7f
Fapen <apfhddlfpt@naver.com>
2020-06-03 20:24:22 +0900

    +-/*

--- C.c
+++ C.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
+  int a, b;
   printf("Hello World\n");
+  scanf("%d %d", &a, &b);
+  printf("%d+%d=%d\n", a,b,a+b);
+  printf("%d-%d=%d\n", a,b,a-b);
+  printf("%d/%d=%d\n", a,b,a/b);
+  printf("%d*%d=%d\n", a,b,a*b);
   return 0;
 }
