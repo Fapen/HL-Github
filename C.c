1cb33ab6e59262f686537efdc772ca100815687b
Fapen <apfhddlfpt@naver.com>
2020-06-03 20:26:39 +0900

    mod

--- C.c
+++ C.c
@@ -4,9 +4,10 @@ int main(void) {
   int a, b;
   printf("Hello World\n");
   scanf("%d %d", &a, &b);
-  printf("%d+%d=%d\n", a,b,a+b);
-  printf("%d-%d=%d\n", a,b,a-b);
-  printf("%d/%d=%d\n", a,b,a/b);
-  printf("%d*%d=%d\n", a,b,a*b);
+  printf("%d + %d= %d\n", a,b,a+b);
+  printf("%d - %d= %d\n", a,b,a-b);
+  printf("%d / %d= %d\n", a,b,a/b);
+  printf("%d * %d= %d\n", a,b,a*b);
+  printf("%d %% %d= %d\n", a,b,a%b);
   return 0;
 }
